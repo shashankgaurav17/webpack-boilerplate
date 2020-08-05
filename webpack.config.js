b5a3fa65fd13a0400032f5ca204827a28912f1ae
SHASHANK GAURAV <shashankgaurav17@gmail.com>
2020-08-05 16:41:19 +0530

    Init

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin  = require("mini-css-extract-plugin")
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpeg|jpg|gif)$/,
+        use: {
+          loader: "file-loader"
+        }
+      },
+      { 
+        test: /\.scss$/, 
+        use: [ "style-loader", "css-loader", "sass-loader" ] 
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFileName: "[id].css"
+    })
+  ]
+};
+
+//MiniCssExtractPlugin is for production, not for development mode
